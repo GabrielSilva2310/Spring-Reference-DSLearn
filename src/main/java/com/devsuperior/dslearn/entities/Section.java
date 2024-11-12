7ae114aaaadad96dd95d6225b29efb7dbf49fd70
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2024-11-12 16:23:20 -0300

    Section

--- src/main/java/com/devsuperior/dslearn/entities/Section.java
+++ src/main/java/com/devsuperior/dslearn/entities/Section.java
@@ -1,10 +1,6 @@
 package com.devsuperior.dslearn.entities;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
-
-import com.devsuperior.dslearn.enums.ResourceType;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,12 +8,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_resource")
-public class Resource {
+@Table(name = "tb_section")
+public class Section {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,95 +22,113 @@ public class Resource {
 	private String description;
 	private Integer position;
 	private String imgUri;
-	private ResourceType type;
-	private String externalLink;
 	
 	@ManyToOne
-	@JoinColumn(name = "offer_id")
-	private Offer offer;
+	@JoinColumn(name = "resource_id")
+	private Resource resource;
 	
-	@OneToMany(mappedBy = "resource")
-	private List<Section> sections=new ArrayList<>();
-	
-	
-	
-	public Resource() {
+	@ManyToOne
+	@JoinColumn(name = "prerequisite_id")
+	private Section prerequisite;
+
+	public Section() {
 	}
 
-	public Resource(Long id, String title, String description, Integer position, String imgUri, ResourceType type,String externalLink, Offer offer) {
+	
+
+	public Section(Long id, String title, String description, Integer position, String imgUri, Resource resource, Section prerequisite) {
 		this.id = id;
 		this.title = title;
 		this.description = description;
 		this.position = position;
 		this.imgUri = imgUri;
-		this.type = type;
-		this.externalLink = externalLink;
-		this.offer = offer;
+		this.resource = resource;
+		this.prerequisite = prerequisite;
 	}
+	
 
 	public Long getId() {
 		return id;
 	}
 
+
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
+
 
 	public String getTitle() {
 		return title;
 	}
 
+
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
+
 
 	public String getDescription() {
 		return description;
 	}
 
+
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
+
+
 
 	public Integer getPosition() {
 		return position;
 	}
 
+
+
 	public void setPosition(Integer position) {
 		this.position = position;
 	}
+
+
 
 	public String getImgUri() {
 		return imgUri;
 	}
 
+
+
 	public void setImgUri(String imgUri) {
 		this.imgUri = imgUri;
 	}
 
-	public ResourceType getType() {
-		return type;
+
+
+	public Resource getResource() {
+		return resource;
 	}
 
-	public void setType(ResourceType type) {
-		this.type = type;
+
+
+	public void setResource(Resource resource) {
+		this.resource = resource;
 	}
 
-	public String getExternalLink() {
-		return externalLink;
+
+
+	public Section getPrerequisite() {
+		return prerequisite;
 	}
 
-	public void setExternalLink(String externalLink) {
-		this.externalLink = externalLink;
+
+
+	public void setPrerequisite(Section prerequisite) {
+		this.prerequisite = prerequisite;
 	}
 
-	public Offer getOffer() {
-		return offer;
-	}
-
-	public void setOffer(Offer offer) {
-		this.offer = offer;
-	}
 
 	@Override
 	public int hashCode() {
@@ -130,13 +143,13 @@ public class Resource {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Resource other = (Resource) obj;
+		Section other = (Section) obj;
 		return Objects.equals(id, other.id);
 	}
 	
 	
 	
 	
-
+	
 
 }
